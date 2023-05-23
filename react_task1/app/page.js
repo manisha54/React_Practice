5c0c12bbe1532e29c6c5655bfc3971b40b2e5ab3
manisha chaudhary <manishachaudhary2057@gmail.com>
2023-05-23 11:45:47 +0545

    make common parent to share same data

--- react_task1/app/page.js
+++ react_task1/app/page.js
@@ -1,13 +1,18 @@
+'use client'
 import Counter from "@/components/counter"
-
+import { useState } from "react"
 
 export default function Home() {
+  const [count, setCount] = useState(0)
+  const handleClick1 =()=> setCount(count+1)
+
   
   return (
     <div>
       <h1>Task 1 -- Counter App</h1>
 
-      <Counter/>
+      <Counter count={count} handleClick1={handleClick1}/>
+      <Counter count={count}handleClick1={handleClick1}/>
       
     </div>
 
