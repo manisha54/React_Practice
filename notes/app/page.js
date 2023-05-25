ab55b0489f95d2ecb143d54fa38a06264e55b9e9
manisha chaudhary <manishachaudhary2057@gmail.com>
2023-05-25 11:48:55 +0545

    updated

--- notes/app/page.js
+++ notes/app/page.js
@@ -1,27 +1,27 @@
-  'use client'
-  import Notes from "@/components/Notes"
-  
-  export default function Home(){
-    const notes =[
-      {
-        id:1,
-        desc:'today is chilli',
-        important :true
-      },
-       {
-        id:2,
-        desc:'css is required for web applications',
-        important :false
-      },
-      {
-        id:3,
-        desc:'js with react is a must',
-        important :true
-      }
-    ]
-    return (
-      <div>
-        <Notes notes ={notes}/>
-      </div>
-    )
-  }
+'use client'
+import Notes from "@/components/Notes"
+
+export default function Home() {
+  const notes = [
+    {
+      id: 1,
+      desc: 'today is chilli',
+      important: true
+    },
+    {
+      id: 2,
+      desc: 'css is required for web applications',
+      important: false
+    },
+    {
+      id: 3,
+      desc: 'js with react is a must',
+      important: true
+    }
+  ]
+  return (
+    <div>
+      <Notes notes={notes} />
+    </div>
+  )
+}
--- notes/components/Notes.js
+++ notes/components/Notes.js
@@ -1,14 +1,58 @@
-export default function Notes(props){
-    const {notes} = props   //object d-structure
+import { useState } from "react"
+
+export default function Notes(props) {
+ //   const { notes } = props   //object d-structure
+    const [desc, setDesc] = useState('')
+    const [notes, setNotes] = useState(props.notes)    // remove line 4 and use props.notes
+    const handleChange = (event) =>{
+        console.log(event.target.value)   //see on the console what you type on text field
+        setDesc(event.target.value)
+    }
+
+    const handleAdd = (event) =>{
+         event.preventDefault()  //to stop send data on another server
+         const newNote ={     //newNote is object
+            id: notes.length+1,
+            desc:desc,
+            important: Math.random() < 0.5
+
+         }
+         console.log(newNote)
+         setNotes(notes.concat(newNote))   //concat to send data after click add button to server(note)(list)
+    }
+
+
+    const handleDelete = (noteId)=>{
+        alert(noteId)
+        
+    }
     return (
         <>
-        <h1> Notes App</h1>
-        <ul>
-            {
-                notes.map(note =>
-                    <li>{note.desc}</li>)
-            }
-        </ul>
+            <h1> Notes App</h1>
+            <ul>
+                
+                {
+                    
+                    notes.map(note => 
+                        <li key={note.id}>
+                            {note.desc}
+                            {' '}
+                            <button onClick={(id) => handleDelete(note.id)}>delete</button>
+                            </li>    // key={note.id }--> remove error from console of server
+                    )   
+                    
+                }
+            </ul>
+            <form>
+                <input type="text" value={desc}
+                onChange={handleChange}
+                />
+                { ' '}
+                <button onClick={handleAdd}>Add</button>
+                
+            </form>
+
+           
         </>
     )
 }
